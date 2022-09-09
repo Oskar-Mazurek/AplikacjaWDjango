170814484dd554ad15f7ebe9d36fb0be284abe22
Oskar-Mazurek <168461@stud.prz.edu.pl>
2022-09-09 15:02:40 +0200

    heroku deploy

--- PycharmProjects/djangoProject/djangoProject/settings.py
+++ PycharmProjects/djangoProject/djangoProject/settings.py
@@ -29,11 +29,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
 # ALLOWED_HOSTS = ['localhost:8000', '127.0.0.1:8000']
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['przychodnia-app.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
